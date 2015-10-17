0c2e9e36e9127e140b3edffe1ad0fd577f4131eb
Francisco Massa <fvsmassa@gmail.com>
2015-10-17 18:19:26 +0200

    Trying to optimize CrossLRN

--- SpatialCrossLRN.lua
+++ SpatialCrossLRN.lua
@@ -12,6 +12,73 @@ function SpatialCrossLRN:__init(size, alpha, beta, k)
 
 end
 
+local function createMatrix(self,numFilters)
+  local mat = torch.Tensor(numFilters, numFilters):zero()
+  local idx = torch.range(1-(self.size-1)/2,numFilters+(self.size-1)/2):long()
+  idx:clamp(1,numFilters)
+  idx = idx:unfold(1,self.size,1)
+  mat:scatter(1,idx:t(),1)
+  return mat
+end
+
+function SpatialCrossLRN:updateOutputFast(input)
+  assert(input:dim() == 3 or input:dim() == 4,
+         'Input must be 3D or 4D')
+  local isBatch = true
+  if input:dim() == 3 then
+    input = nn.utils.addSingletonDimension(input)
+    isBatch = false
+  end
+
+  local tt = torch.Timer()
+  local batchSize   = input:size(1)
+  local channels    = input:size(2) 
+  local inputHeight = input:size(3) 
+  local inputWidth  = input:size(4) 
+
+  self.output:resizeAs(input)
+
+tt:reset()
+  -- use output storage as temporary buffer
+  local inputSquare = self.output
+  inputSquare:pow(input:transpose(2,3):transpose(3,4), 2):resize(input:size(1)*input:size(3)*input:size(4),input:size(2))
+print('input square non cont',tt:time().real)
+tt:reset()
+  self.cmat = createMatrix(self,input:size(2))
+
+print('create matrix', tt:time().real)
+
+  --self.scale:resizeAs(input)
+  self.scale:resizeAs(inputSquare):fill(self.k)
+
+tt:reset()
+  self.scale:addmm(self.alpha/self.size,inputSquare,self.cmat)
+
+print('addmm',tt:time().real)
+
+  --self.scale:mul(self.alpha/self.size):add(self.k)
+
+tt:reset()
+local cc = self.scale:view(input:size(1),input:size(3),input:size(4),input:size(2)):transpose(2,4):transpose(3,4):clone()
+
+print('view+clone',tt:time().real)
+tt:reset()
+  self.output:pow(cc,-self.beta)
+
+print('pow -beta',tt:time().real)
+tt:reset()
+  self.output:cmul(input)
+print('cmul',tt:time().real)
+
+  if not isBatch then
+    self.output = self.output[1]
+  end
+
+  return self.output
+end
+
+
+
 function SpatialCrossLRN:updateOutput(input)
   assert(input:dim() == 3 or input:dim() == 4,
          'Input must be 3D or 4D')
